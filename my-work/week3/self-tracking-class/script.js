c6cb556f7703f1405c683f3bc20684552ac97d37
Zihan (Sumner) Feng <37652167+baoatwork@users.noreply.github.com>
2020-03-10 23:34:49 +0800

    add classes and groups to the self checking project!

--- my-work/week3/self-tracking-class/script.js
+++ my-work/week3/self-tracking-class/script.js
@@ -12,14 +12,26 @@ let viz = d3.select("#viz-container")
 function gotData(newData){
   console.log(newData);
 
-   let draw= viz.selectAll("rect").data(newData).enter()
+   let draw= viz.selectAll(".happyStick").data(newData).enter()
+              .append("g")
+              .attr("class","happyStick");
 
-   draw.append("rect")
+
+
+     draw.append("rect")
      .attr("x",xPosition)
      .attr("y",yPosition)
      .attr("width",10)
      .attr("height",150)
      .attr("fill",whatColor)
+
+  ;
+
+    draw.append("circle")
+    .attr("cx",xPosition)
+    .attr("cy",yPosition)
+    .attr("r",whatRadius)
+    .attr("fill",whatColorCircle)
 
   ;
 }
@@ -54,5 +66,30 @@ function yPosition(sample){
     return 210;
   }else{
     return 410;
+  }
+}
+
+function whatRadius(sample){
+  check = sample.container;
+
+  if(check == "Plastic Bottle"){
+    return 5;
+  }else if(check == "Cup"){
+    return 10;
+  }else{
+    return 20;
+  }
+}
+
+
+function whatColorCircle(sample){
+  check = sample.heat;
+
+  if (check == "Cold"){
+    return "#434343";
+  }else if (check == "Warm"){
+    return "#FFBA84";
+  }else {
+    return "#AB3B3A";
   }
 }
